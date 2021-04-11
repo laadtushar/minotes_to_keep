c1d63a0aadfa02b37850036b8196be1ce2521b12
laadtushar <81948354+laadtushar@users.noreply.github.com>
2021-04-12 01:04:42 +0530

    Create Main.py
    This is the main code file

---
+++ Main.py
@@ -0,0 +1,46 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+
+#PATH = "C:\Program Files (x86)\chromedriver.exe"
+chrome_options = Options()
+chrome_options.add_argument("user-data-dir=C:\\Users\\91940\\AppData\\Local\\Google\\Chrome\\User Data\\Default") # change to profile path
+chrome_options.add_argument('--profile-directory=Default')
+driver = webdriver.Chrome(executable_path="C:\\Program Files (x86)\\chromedriver.exe", chrome_options=chrome_options)
+
+username = "enterusername"
+password = "enterpasword"
+
+#driver.get("https://us.i.mi.com/")
+
+driver.get("https://account.xiaomi.com/pass/serviceLogin?callback=https%3A%2F%2Fus.i.mi.com%2Fsts%3Fsign%3DQUjUnrCggg9No240fiSu7mm%252BaPw%253D%26followup%3Dhttps%253A%252F%252Fus.i.mi.com%252F%26sid%3Di.mi.com&sid=i.mi.com&_locale=en_US&_snsNone=true")
+
+""" time.sleep(20)
+
+
+username_text = driver.find_element_by_name("account")
+username_text.send_keys(username)
+password_text = driver.find_element_by_name("password")
+password_text.send_keys(password)
+
+login_button = driver.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div/div[2]/div[2]/div[2]/div[3]/div[1]/form/div/button")
+login_button.submit()
+WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div[3]/a[4]/div[1]/img'))).click()
+WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="verify-mod-send_ticket_tip"]/div[2]/button'))).click()
+
+otp = input("Enter OTP:" )
+
+otp_text = driver.find_element_by_name("ticket")
+otp_text.send_keys(otp)
+WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="verify-mod-SMS"]/div[3]/a'))).click()
+
+o = []
+listdiv = driver.find_elements_by_class_name("")
+
+for content in listdiv:
+    o.append(content) """
